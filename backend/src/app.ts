67018ca00eabf5fc08a161998a1fe6b9a3cf21aa
Zoltan Berente <berentez07b@gmail.com>
2021-08-29 17:13:54 +0200

    BA working environment

---
+++ backend/src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+import { api } from "./routes";
+import errorHandler from "./middlewares/error-handler";
+
+const app = express();
+
+app.use("/api", api);
+
+app.use(errorHandler);
+
+export default app;
