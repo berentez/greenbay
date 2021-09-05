de1dd69266872829d990f76e97b1b302ca593030
Zoltan Berente <berentez07b@gmail.com>
2021-09-05 19:10:55 +0200

    login controller added

---
+++ backend/src/controllers/login-controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express';
+import HttpException from '../exceptions/http-exception';
+import { ErrorHandling, LoginReq } from '../models';
+import { userService } from '../services';
+
+export const loginController = {
+  async post(
+    req: Request<LoginReq>,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    const { username, password } = req.body;
+    const data = await userService
+      .login({ username, password })
+      .catch(error => {
+        next(new HttpException(500, error));
+      });
+    if (
+      (data as ErrorHandling).message === 'Username or password is incorrect.'
+    ) {
+      res.status(401).json(data);
+    } else if ((data as ErrorHandling).status === 'error') {
+      res.status(400).json(data);
+    } else {
+      res.status(200).json(data);
+    }
+  },
+};
--- backend/src/routes/api.routes.ts
+++ backend/src/routes/api.routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 import { registrationController } from '../controllers';
+import { loginController } from '../controllers/login-controller';
 
 const router = express.Router();
 
@@ -9,5 +10,6 @@ router.use(cors());
 router.use(express.json());
 
 router.post('/registration', registrationController.post);
+router.post('/login', loginController.post);
 
 export default router;
