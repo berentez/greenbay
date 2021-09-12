ac8cb7d4177481186be020fbe81af5b7b963a7ae
Zoltan Berente <berentez07b@gmail.com>
2021-09-12 16:36:13 +0200

    book table created

---
+++ backend/migrations/sqls/20210912142322-add-book-up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `greenbook`.`book` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `author` VARCHAR(45) NOT NULL,
+  `title` VARCHAR(90) NOT NULL,
+  `page` INT NOT NULL,
+  `color` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
